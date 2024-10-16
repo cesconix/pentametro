bb9a77ad0d9874cc9e44c5b47fe056252cbf3960
Francesco Pasqua <cesconix@me.com>
2024-10-16 09:17:07 +0200

    feat: add clickable logo

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,8 +1,16 @@
 import { GlassesIcon } from "lucide-react"
 
 export const Logo = () => (
-  <h1 className="flex items-center gap-1.5 text-primary">
-    <GlassesIcon strokeWidth={2.2} className="w-6 h-6" />
-    <span className="text-xl font-logo">Pentametro</span>
-  </h1>
+  <a
+    href="/"
+    className="flex items-center text-primary group hover:scale-105 transition"
+  >
+    <h1 className="flex items-center gap-1.5">
+      <GlassesIcon
+        strokeWidth={2.2}
+        className="w-6 h-6 group-hover:-rotate-12 transition"
+      />
+      <span className="text-xl font-logo">Pentametro</span>
+    </h1>
+  </a>
 )
